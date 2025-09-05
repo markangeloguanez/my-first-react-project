f26ce6ffee299a024f062c3c9979cc3e2d05a2c2
markangeloguanez <markangeloguanez@gmail.com>
2025-09-04 21:43:53 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { CORE_CONCEPTS } from "./data.js";
+import Header from './components/Header/Header.jsx';
+import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
+import { TabButton } from "./components/TabButton.jsx";
+
+function App() {
+  function handleConcept() {
+    alert("show concept content!");
+  };
+
+  function handleJSX() {
+    alert("Show JSX content!");
+  }
+
+  function handleProps() {
+    alert("Show Props content!");
+  }
+
+  function handleState() {
+    alert("Show State ocntent!");
+  }
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+        <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onClick={handleConcept}>Components</TabButton>
+            <TabButton onClick={handleJSX}>JSX</TabButton>
+            <TabButton onClick={handleProps}>Props</TabButton>
+            <TabButton onClick={handleState}>State</TabButton>
+          </menu>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TabButton.jsx
@@ -0,0 +1,5 @@
+export function TabButton({children, onClick}) {
+    return (
+        <li><button onClick={onClick}>{children}</button></li>
+    );
+}
